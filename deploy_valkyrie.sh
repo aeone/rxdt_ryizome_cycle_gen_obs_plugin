ca35f23e0db1fdaaa54418018fd45ad4a14e19c8
Ryi <ryielle@icloud.com>
2023-10-14 21:10:52 +0100

    Minor fixups

--- deploy_valkyrie.sh
+++ deploy_valkyrie.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+# don't forget to npm run dev
 rsync -rv --exclude=.git --exclude=node_modules /Users/ryielle/Projects/rxdt_ryizome_cycle_gen_obs_plugin /Users/ryielle/Ryizome/.obsidian/plugins # rxdt sync for valkyrie
