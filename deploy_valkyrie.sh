77496bd4978cede3a7259ab99496c3ba1a09163c
Ryi <ryielle@icloud.com>
2023-10-14 19:52:20 +0100

    Initial deployment trial

---
+++ deploy_valkyrie.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+rsync -rv --exclude=.git --exclude=node_modules /Users/ryielle/Projects/rxdt_ryizome_cycle_gen_obs_plugin /Users/ryielle/Ryizome/.obsidian/plugins # rxdt sync for valkyrie
